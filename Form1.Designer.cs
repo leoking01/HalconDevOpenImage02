263880c8b986c754af58c9632f0c36e8f452d71c
he <leoking02@foxmail.com>
2019-12-09 09:37:11 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace HalconDevOpenImage02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hSmartWindowControl1 = new HalconDotNet.HSmartWindowControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hSmartWindowControl1
+            // 
+            this.hSmartWindowControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.hSmartWindowControl1.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.hSmartWindowControl1.HDoubleClickToFitContent = true;
+            this.hSmartWindowControl1.HDrawingObjectsModifier = HalconDotNet.HSmartWindowControl.DrawingObjectsModifier.None;
+            this.hSmartWindowControl1.HImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hSmartWindowControl1.HKeepAspectRatio = true;
+            this.hSmartWindowControl1.HMoveContent = true;
+            this.hSmartWindowControl1.HZoomContent = HalconDotNet.HSmartWindowControl.ZoomContent.WheelForwardZoomsIn;
+            this.hSmartWindowControl1.Location = new System.Drawing.Point(9, 9);
+            this.hSmartWindowControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.hSmartWindowControl1.Name = "hSmartWindowControl1";
+            this.hSmartWindowControl1.Size = new System.Drawing.Size(512, 512);
+            this.hSmartWindowControl1.TabIndex = 0;
+            this.hSmartWindowControl1.WindowSize = new System.Drawing.Size(512, 512);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "readImage";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(650, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "imageProc";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 531);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hSmartWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HSmartWindowControl hSmartWindowControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using HalconDotNet;
+
+namespace HalconDevOpenImage02
+{
+    public partial class Form1 : Form
+    {
+        HDevelopExport hd = new HDevelopExport();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //readImgae
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hd.ReadPicture(hSmartWindowControl1.HalconWindow);
+        }
+
+        //imageProc
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hd.Process();
+        }
+    }
+
+    public class HDevelopExport
+    {
+        public HTuple hv_ExpDefaultWinHandle;
+        HObject ho_Image, ho_ImageGray;
+        HTuple hv_Width, hv_Height;
+        //初始化halcon
+        public void InitHalcon()
+        {
+            // Default settings used in HDevelop 
+            HOperatorSet.SetSystem("do_low_error", "false");
+        }
+        public void ReadPicture(HTuple Window)
+        {
+            //读图并显示
+            hv_ExpDefaultWinHandle = Window;
+            //HOperatorSet.GenEmptyObj(out ho_Image);
+            //ho_Image.Dispose();
+            HOperatorSet.ReadImage(out ho_Image, Application.StartupPath +"/../../1.jpg");
+            HTuple wid,hei;
+            HOperatorSet.GetImageSize( ho_Image, out wid, out hei  );
+            HOperatorSet.SetPart(hv_ExpDefaultWinHandle, 0, 0, hei, wid  );
+            HOperatorSet.DispObj(ho_Image, hv_ExpDefaultWinHandle);
+            HOperatorSet.GetImageSize(ho_Image, out hv_Width, out hv_Height);
+        }
+        public void Process()
+        {
+            //处理程序
+            //HOperatorSet.GenEmptyObj(out ho_ImageGray);
+            //ho_ImageGray.Dispose();
+            //HOperatorSet.Rgb3ToGray(ho_Image, ho_Image, ho_Image, out ho_ImageGray);
+            HOperatorSet.Rgb1ToGray(ho_Image  , out ho_ImageGray);
+            HOperatorSet.DispObj(ho_ImageGray, hv_ExpDefaultWinHandle);
+            //ho_Image.Dispose();
+            //ho_ImageGray.Dispose();
+        }
+    }
+}
